37825e70d772030d54cea6f10436f26b84f896c4
StarHeartHunt <starheart233@gmail.com>
2024-05-08 18:16:46 +0800

    :arrow_up: upgrade sponsorkit

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,55 +1,56 @@
-import { BadgePreset, defineConfig, presets } from "sponsorkit";
+import { defineConfig } from "sponsorkit";
 
-const past: BadgePreset = {
-  avatar: {
-    size: 20,
-  },
-  boxWidth: 22,
-  boxHeight: 22,
-  container: {
-    sidePadding: 35,
-  },
-};
+const year = new Date().getFullYear();
+const month = new Date().getMonth() + 1;
+
+const date = `${year}-${month.toString().padStart(2, "0")}`;
 
 export default defineConfig({
-  tiers: [
+  outputDir: ".",
+  formats: ["svg", "png"],
+
+  renders: [
     {
-      title: "Past Sponsors",
-      monthlyDollars: -1,
-      preset: past,
+      name: `sponsors.${date}`,
+      width: 1000,
+      includePastSponsors: false,
+      renderer: "circles",
     },
     {
-      title: "Backers",
-      preset: presets.small,
+      name: "sponsors.all",
+      width: 1000,
+      includePastSponsors: true,
+      renderer: "circles",
     },
     {
-      title: "Sponsors",
-      monthlyDollars: 5,
-      preset: {
-        avatar: {
-          size: 42,
-        },
-        boxWidth: 52,
-        boxHeight: 52,
-        container: {
-          sidePadding: 30,
-        },
+      name: "sponsors",
+      width: 800,
+      includePastSponsors: true,
+      renderer: "tiers",
+    },
+    {
+      name: "sponsors.wide",
+      width: 1800,
+      includePastSponsors: true,
+      renderer: "tiers",
+    },
+    {
+      name: "sponsors.part1",
+      width: 800,
+      includePastSponsors: true,
+      renderer: "tiers",
+      filter(sponsor) {
+        return sponsor.monthlyDollars >= 9.9;
       },
     },
     {
-      title: "Silver Sponsors",
-      monthlyDollars: 10,
-      preset: presets.medium,
-    },
-    {
-      title: "Gold Sponsors",
-      monthlyDollars: 25,
-      preset: presets.large,
-    },
-    {
-      title: "Platinum Sponsors",
-      monthlyDollars: 50,
-      preset: presets.xl,
+      name: "sponsors.part2",
+      width: 800,
+      includePastSponsors: true,
+      renderer: "tiers",
+      filter(sponsor) {
+        return sponsor.monthlyDollars < 9.9;
+      },
     },
   ],
 });
